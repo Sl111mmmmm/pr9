b0bec1f29f6fa1e7763be07d2c3303875cd2ea27
Sl111mmmmm <danon1weeee@gmail.com>
2024-10-12 13:14:17 +0500

    добавил ответ на задание 5

---
+++ homework5/5.py
@@ -0,0 +1,27 @@
+numbers = []
+
+
+while True:
+    user_input = input("Введите число (или 'end' для завершения): ")
+    if user_input.lower() == 'end':
+        break
+    try:
+
+        number = int(user_input)
+        numbers.append(number)
+    except ValueError:
+        print("Ошибка: введите корректное целое число.")
+
+
+if numbers:
+
+    min_index = numbers.index(min(numbers))
+    max_index = numbers.index(max(numbers))
+
+
+    numbers[min_index], numbers[max_index] = numbers[max_index], numbers[min_index]
+
+
+    print("Список после замены:", numbers)
+else:
+    print("Список пуст.")
