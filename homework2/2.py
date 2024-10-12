231d19fe9d3c2ca4f319c05e1de483d2c5a6058e
Sl111mmmmm <danon1weeee@gmail.com>
2024-10-12 13:02:15 +0500

    добавил ответ на задание 2

--- homework2/2.py
+++ homework2/2.py
@@ -1,15 +1,29 @@
-import math
+def intInput(s1):
+    while True:
+        try:
+            return int(input(s1))
+        except ValueError:
+            print("Некорректный ввод, введите целое число.")
 
-a = float(input("Введите число a: "))
-b = float(input("Введите число b: "))
+def squaresBetween(a, b):
+    start = min(a, b) + 1
+    end = max(a, b)
+    squares = [i**2 for i in range(start, end)]
+    return squares
 
+def differentNum():
+    while True:
+        a = intInput("Введите число (a): ")
+        b = intInput("Введите число (b): ")
+        if a != b:
+            return a, b
+        else:
+            print("Числа не должны быть одинаковыми. Введите, разные числа.")
 
-if a > b:
-    a, b = b, a  
+a, b = differentNum()
 
-a_int = math.ceil(a)
-b_int = math.floor(b)
-
-squares = [i**2 for i in range(a_int, b_int + 1)]
-
-print("Квадраты целых чисел между", a, "и", b, ":", squares)
+squares = squaresBetween(a, b)
+if squares:
+    print("Квадраты чисел между", a, "и", b, ":", squares)
+else:
+    print("Между числами", a, "и", b, "нет целых чисел для возведения в квадрат.")
